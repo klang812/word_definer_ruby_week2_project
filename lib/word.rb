c9ac0be77115e4dad134840dbda66cac25d8d77d
Kerry Lang <klang812@gmail.com>
2021-03-05 08:28:32 -0800

    add all method test to pass

--- lib/word.rb
+++ lib/word.rb
@@ -1,2 +1,18 @@
-class word
+class Word
+  attr_reader :id, :word
+
+  @@words = {}
+
+  def initialize(word, id)
+    @word = word
+    @id = id
+  end
+
+  def self.all
+    @@words.values()
+  end
   
+
+
+
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,2 +1,11 @@
 require 'rspec'
 require 'word'
+
+describe '#Word' do
+  
+  describe('.all') do
+    it("returns an empty array when there are no words") do
+      expect(Word.all).to(eq([]))
+    end
+  end
+end
