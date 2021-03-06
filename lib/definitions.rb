694067f0a23e02ff352712abcecd2892884d540a
Kerry Lang <klang812@gmail.com>
2021-03-05 16:22:06 -0800

    add == method for definitions test to pass

--- lib/definitions.rb
+++ lib/definitions.rb
@@ -1,45 +1,19 @@
 class Definitions
   attr_reader :id
-  attr_accessor :word, :word_id
+  attr_accessor :definition, :word
 
   @@definitions = {}
   @@id = 0
 
-  def initialize(definition, word_id, id)
+  def initialize(definition, word, id)
     @definition = definition
-    @word_id = word_id
+    @word = word
     @id = id || @@id += 1
   end
 
   def ==(compare_definition)
-    (self.definition() == compare_definition.definition()) && (self.word_id() == compare_definition.word_id())
+    (self.definition() == compare_definition.definition()) && (self.word() == compare_definition.word())
   end
-
-  def self.all
-    @@definitions.values
-  end
-
-  def save
-    @@definitions[self.id] = Definitions.new(self.definition, self.word_id, self.id)
-  end
-
-  def self.find(id)
-    @@definitions[id]
-  end
-
-  def update(word, word_id)
-    self.word = word
-    self.word_id = word_id
-    @@definitions[self.id] = Definitions.new(self.definition, self.word_id, self.id)
-  end
-
-  def delete
-    @@definitions.delete(self.id)
-  end
-
-  def self.clear
-    @@definitions = {}
-  end
-end
 
   
+end 
--- spec/definitions_spec.rb
+++ spec/definitions_spec.rb
@@ -6,8 +6,6 @@ require 'pry'
 describe '#Definitions' do
   
   before(:each) do
-    Word.clear()
-    Definitions.clear()
     @word = Word.new("star", nil)
     @word.save()
   end
@@ -19,63 +17,5 @@ describe '#Definitions' do
       expect(definition).to(eq(definition2))
     end
   end
-
-  describe('.all') do
-    it("returns a list of all definitions") do
-      definition = Definitions.new("bright", @word.id, nil)
-      definition.save()
-      definition2 = Definitions.new("not good", @word.id, nil)
-      definition2.save()
-      expect(Definitions.all).to(eq([definition, definition2]))
-    end
-  end
-
-  describe('.clear') do
-    it("clears all definitions") do
-      definition = Definitions.new("bright", @word.id, nil)
-      definition.save()
-      definition2 = Definitions.new("not good", @word.id, nil)
-      definition2.save()
-      Definitions.clear()
-      expect(Definitions.all).to(eq([]))
-    end
-  end
-
-  describe('#save') do
-    it("saves a definition") do
-      definition = Definitions.new("bright", @word.id, nil)
-      definition.save()
-      expect(Definitions.all).to(eq([definition]))
-    end
-  end
-
-  describe('.find') do
-    it("finds a definition by id") do
-      definition = Definitions.new("bright", @word.id, nil)
-      definition.save()
-      definition2 = Definitions.new("not good", @word.id, nil)
-      definition2.save()
-      expect(Definitions.find(definition2.id)).to(eq(definition2))
-    end
-  end
-
-  describe('#update') do
-    it("updates a definition by id") do
-      definition = Definitions.new("bright", @word.id, nil)
-      definition.save()
-      definition.update("supernova", @word.id)
-      expect(definition.word).to(eq("supernova"))
-    end
-  end
-
-  describe('#delete') do
-    it("deletes a definition by id") do
-      definition = Definitions.new("bright", @word.id, nil)
-      definition.save()
-      definition2 = Definitions.new("not good", @word.id, nil)
-      definition2.save()
-      definition.delete()
-      expect(Definitions.all).to(eq([definition2]))
-    end
-  end
 end
+ 
